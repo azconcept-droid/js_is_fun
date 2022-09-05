89f87029b1fa7127a485e22ea9f441758a574497
Yahaya <azconcept2016@gmail.com>
2022-09-05 18:40:17 +0100

    feat: comment out practice examples

--- beginner-html-site-styled-gh-pages/beginner-html-site-styled-gh-pages/scripts/main.js
+++ beginner-html-site-styled-gh-pages/beginner-html-site-styled-gh-pages/scripts/main.js
@@ -1,16 +1,17 @@
-/*Hello World*/
+/*
+//Hello World
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
 
-/*Event Listener*/
+// Event Listener
 const myHtml = document.querySelector('html');
 myHtml.addEventListener('click', function () {
     alert('Ouch! Stop poking me!');
   });
 
-/*Event listner with arrow function */
+//Event listner with arrow function 
 const myHTML = document.querySelector('img');
 myHTML.addEventListener('click', () => {
     alert('Ouch! I am an image!');
 });
-  
+*/
