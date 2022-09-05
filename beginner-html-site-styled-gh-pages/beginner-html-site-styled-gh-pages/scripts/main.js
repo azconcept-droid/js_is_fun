ad8aa9a641c615318e5daf96fd67e5501c40f12b
Yahaya <azconcept2016@gmail.com>
2022-09-05 18:38:04 +0100

    feat: add event listener

--- beginner-html-site-styled-gh-pages/beginner-html-site-styled-gh-pages/scripts/main.js
+++ beginner-html-site-styled-gh-pages/beginner-html-site-styled-gh-pages/scripts/main.js
@@ -1,2 +1,16 @@
+/*Hello World*/
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
+
+/*Event Listener*/
+const myHtml = document.querySelector('html');
+myHtml.addEventListener('click', function () {
+    alert('Ouch! Stop poking me!');
+  });
+
+/*Event listner with arrow function */
+const myHTML = document.querySelector('img');
+myHTML.addEventListener('click', () => {
+    alert('Ouch! I am an image!');
+});
+  
