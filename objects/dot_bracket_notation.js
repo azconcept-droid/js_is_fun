a642547c86e14c9fb21d71f44b3a948a807ec1e2
Yahaya Azeez <azconcept2016@gmail.com>
2023-10-04 12:10:00 +0100

    add: accessing an object property

---
+++ objects/dot_bracket_notation.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const azeez = {
+    firstname: 'Azeez',
+    lastname: 'Yahaya',
+    age: 2040 - 1991,
+    job: 'programmer',
+    wife: 'Ajoke',
+    friends: ['Mr. Kazeem', 'Olalere', 'Jamiu',],
+}
+
+console.log(azeez)
+
+console.log(azeez.wife)
+console.log(azeez['job'])
+
+const nameKey = 'name'
+console.log(azeez['first' + nameKey], azeez['last' + nameKey])
+
+const interestedIn = prompt('What do you want to know about Azeez: Choose between \
+firstName, lastNmae, age, job, wifeName, and friends')
+
+console.log(azeez[interestedIn])
+
+if (azeez['interestedIn'] === true) {
+    console.log(azeez['interestedIn'])
+}
+// } else {
+//     console.log('Enter a valid input')
