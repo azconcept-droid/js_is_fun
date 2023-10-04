3c4204176aff2e5c1345c18e4b42ca769938c815
Yahaya Azeez <azconcept2016@gmail.com>
2023-10-04 18:16:04 +0100

    last commit

--- objects/dot_bracket_notation.js
+++ objects/dot_bracket_notation.js
@@ -17,16 +17,22 @@ console.log(azeez['job'])
 const nameKey = 'name'
 console.log(azeez['first' + nameKey], azeez['last' + nameKey])
 
-const interestedIn = prompt('What do you want to know about Azeez: Choose between \
-firstName, lastNmae, age, job, wifeName, and friends')
+// const interestedIn = prompt('What do you want to know about Azeez: Choose between \
+// firstName, lastNmae, age, job, wifeName, and friends')
 
-if (azeez[interestedIn]) {
-    console.log(azeez[interestedIn])
-} else {
-     console.log('Enter a valid input')
-}
+// if (azeez[interestedIn]) {
+//     console.log(azeez[interestedIn])
+// } else {
+//      console.log('Enter a valid input')
+// }
 
 azeez.location = 'Nigeria'
 azeez['twitter'] = '@yalecttech'
 
 console.log(azeez)
+
+// coding challenge
+let number_of_friends = azeez.friends.length, person = azeez.firstname
+let best_friend = azeez.friends[0]
+let message = `${person} has ${number_of_friends} friends, and his best friend is called ${best_friend}`
+console.log(message)
