445e181828f96e271adb0d20967aac838cf5e471
Yahaya Azeez <azconcept2016@gmail.com>
2023-10-05 15:33:11 +0100

    add getSummary function

--- objects/object_methods.js
+++ objects/object_methods.js
@@ -7,6 +7,7 @@ const azeez = {
     job: 'programmer',
     wife: 'Ajoke',
     friends: ['Mr. Kazeem', 'Olalere', 'Jamiu',],
+    hasDriversLicense: true,
     // calcAge: function () {
     //     console.log(this)
     //     return 2037 - this.birthyear
@@ -14,11 +15,14 @@ const azeez = {
     calcAge: function () {
         this.age = 2037 - this.birthyear
         return this.age
+    },
+    getSummary: function () {
+        let aNo = (this.hasDriversLicense) ? a : no
+        let summary = `${this.firstname} is a ${this.age}-year old ${job}, and he has ${aNo} drivers's license`
+        return summary
     }
 }
 
 console.log(azeez.calcAge())
-console.log(azeez.calcAge())
-console.log(azeez.calcAge())
-console.log(azeez.calcAge())
+console.log(azeez.getSummary())
 
