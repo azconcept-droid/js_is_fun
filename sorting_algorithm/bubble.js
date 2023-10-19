714064f2a193bb8b5efd1f2b2543e99eb784b481
Yahaya Azeez <azconcept2016@gmail.com>
2023-10-19 17:35:34 +0100

    fix bug :bug:

--- sorting_algorithm/bubble.js
+++ sorting_algorithm/bubble.js
@@ -1,22 +1,27 @@
 let array = [1, 100, -5, -2, 89]
 
-let i = 0, j, temp;
-function swap(a, b) {
-    temp = a
-    a = b
-    b = temp
-}
+// function swap(a, b) {
+//     let temp = a
+//     // console.log(a, b)
+//     a = b
+//     b = temp
+//     // console.log(a, b)
+// }
 
-function sort(i, arrayLength, j) {
-    while (i < arrayLength) {
-        for(j = 0; j < arrayLength; j++) {
+function sort(array) {
+    let i = 0, j;
+    while (i < array.length) {
+        for(j = 0; j < array.length; j++) {
             if (array[j] > array[j+1]) {
-                swap(array[j], array[j+1])
+                // swap(array[j], array[j+1])
+                console.log(array)
+                let temp = array[j]
+                array[j] = array[j+1]
+                array[j+1] = temp
             }
         }
         i++
     }
-
-    return array
 }
-console.log(sort(i, array.length, j))
+sort(array)
+console.log(array)
