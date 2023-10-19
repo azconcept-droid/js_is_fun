c4930ba3322ffdf63237e34b42ceaf23068299b9
Yahaya Azeez <azconcept2016@gmail.com>
2023-10-19 16:51:56 +0100

    add a swapping function

--- sorting_algorithm/bubble.js
+++ sorting_algorithm/bubble.js
@@ -1,13 +1,15 @@
 let array = [1, 100, -5, -2, 89]
 
 let i = 0, j, temp;
-
+function swap(a, b) {
+    temp = a
+    a = b
+    b = temp
+}
 while (i < array.length) {
     for(j = 0; j < array.length; j++) {
         if (array[j] > array[j+1]) {
-            temp = array[j]
-            array[j] = array[j+1]
-            array[j+1] = temp
+            swap(array[j], array[j+1])
         }
     }
     i++
