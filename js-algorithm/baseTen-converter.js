24903812f78abd1b85534ae5075e5c6e388a21cc
Yahaya Azeez <azconcept2016@gmail.com>
2023-10-28 11:24:41 +0100

    convert fro base 10 to another base

---
+++ js-algorithm/baseTen-converter.js
@@ -0,0 +1,14 @@
+ function baseConverter(base, number) {
+    return number.toString(base)
+ }
+
+ console.log("############ BASE 10 ###############")
+console.log(baseConverter(10, 2))
+console.log(baseConverter(10, 12))
+console.log(baseConverter(10, 89))
+console.log("############ BASE 16 ###############")
+console.log(baseConverter(16, 2))
+console.log(baseConverter(16, 12))
+console.log(baseConverter(16, 89))
+
+
