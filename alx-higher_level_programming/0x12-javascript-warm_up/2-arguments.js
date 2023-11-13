92993b9fd09d86b376505f98b1c4882400e376c3
Yahaya Azeez <azconcept2016@gmail.com>
2023-11-13 13:24:21 +0100

    collecting input from command line

---
+++ alx-higher_level_programming/0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const argv = process.argv
+
+if (argv.length <= 2) {
+	console.log("No argument")
+} else if (argv.length === 3) {
+	console.log("Argument found")
+} else if (argv.length >= 4) {
+	console.log("Arguments found")
+}
